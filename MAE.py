correct validation loss normalization improved depth validation only l2 loss for depth

MAE.py
@@ -508,8 +508,8 @@ class MAE:
         loss_val_reset = val_loss.assign(0)
         loss_val_update = val_loss.assign_add(loss/1080.)
 
-        sum_epoch_loss = tf.summary.scalar('Epoch Loss Depth Channel',epoch_loss)
-        sum_val_loss = tf.summary.scalar('Validation Loss Depth Channel',val_loss)
+        sum_epoch_loss = tf.summary.scalar('Epoch Loss Full Model',epoch_loss)
+        sum_val_loss = tf.summary.scalar('Validation Loss Full Model',val_loss)
 
         optimizer1 = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(cost,var_list=[self.full_ec_layer['weights'],
                                                                                                       self.full_ec_layer['bias'],
@@ -715,7 +715,7 @@ class MAE:
                 print('SAVED MODEL')
 
 
-    def validate_model(self,n_validations,loadmodel=True):
+    def validate_model(self,n_validations,run,loadmodel=True):
 
         with tf.Session() as sess:
 
@@ -733,7 +733,8 @@ class MAE:
             #init_op = tf.initialize_all_variables()
             saver = tf.train.Saver()
             if loadmodel == True:
-                saver.restore(sess,self.FLAGS.train_dir+'/MAE.ckpt')
+                dir = self.project_dir + self.folder_model + self.mode + run +
+                saver.restore(sess,dir)
 
             #sess.run(init_op)
 

PretrainingMAE.py
@@ -922,20 +922,10 @@ class PretrainingMAE():
         print('Depth Pretraining')
 
         pred = self.AE_depth(self.input_depth)
-        cost = tf.nn.l2_loss(tf.multiply(self.depth_mask,pred)-tf.multiply(self.depth_mask,self.label_depth)) + \
-               tf.losses.huber_loss(tf.multiply(self.depth_mask,self.label_depth),tf.multiply(self.depth_mask,pred)) + \
-               10*tf.nn.l2_loss(tf.multiply(self.depth_mask,tf.multiply(self.depth_loss_mask,pred)) -
-                                tf.multiply(self.depth_mask,tf.multiply(self.depth_loss_mask,self.label_depth))) + \
-               10*tf.losses.huber_loss(tf.multiply(self.depth_mask,tf.multiply(self.depth_loss_mask,self.label_depth)),
-                                       tf.multiply(self.depth_mask,tf.multiply(self.depth_loss_mask,pred)))
+        cost = tf.nn.l2_loss(tf.multiply(self.depth_mask,pred)-tf.multiply(self.depth_mask,self.label_depth))
 
 
-        loss = tf.nn.l2_loss(tf.multiply(self.depth_mask,pred)-tf.multiply(self.depth_mask,self.label_depth)) + \
-               tf.losses.huber_loss(tf.multiply(self.depth_mask,self.label_depth),tf.multiply(self.depth_mask,pred)) + \
-               10*tf.nn.l2_loss(tf.multiply(self.depth_mask,tf.multiply(self.depth_loss_mask,pred)) -
-                                tf.multiply(self.depth_mask,tf.multiply(self.depth_loss_mask,self.label_depth))) + \
-               10*tf.losses.huber_loss(tf.multiply(self.depth_mask,tf.multiply(self.depth_loss_mask,self.label_depth)),
-                                       tf.multiply(self.depth_mask,tf.multiply(self.depth_loss_mask,pred)))
+        loss = tf.nn.l2_loss(tf.multiply(self.depth_mask,pred)-tf.multiply(self.depth_mask,self.label_depth))
 
         regularizer = tf.contrib.layers.l2_regularizer(scale=1e-05)
         reg = tf.contrib.layers.apply_regularization(regularizer,weights_list=[self.depth_ec_layer['weights'],
@@ -974,7 +964,7 @@ class PretrainingMAE():
         epoch_loss_update = epoch_loss.assign_add(cost)
 
         loss_val_reset = val_loss.assign(0)
-        loss_val_update = val_loss.assign_add(loss/1080.)
+        loss_val_update = val_loss.assign_add(loss)
 
 
         config = tf.ConfigProto(log_device_placement=False)
@@ -1019,9 +1009,11 @@ class PretrainingMAE():
 
                 sess.run(loss_val_reset)
 
+                normalization = self.input_size*set_val.shape[0]
                 for i in set_val:
                     depth_label = self.depth_val[i]
                     depth_mask = self.depth_mask_val[i]
+                    normalization = normalization - np.count_nonzero(depth_mask)
                     depth_loss_mask = self.depth_loss_mask_val[i]
                     depth_in = pretraining_input_distortion(copy(depth_label),singleframe=True)
 
@@ -1038,7 +1030,7 @@ class PretrainingMAE():
 
                 sum_val = sess.run(sum_val_loss)
                 train_writer1.add_summary(sum_val,epoch)
-                print('Validation Loss (per pixel): ', sess.run(val_loss.value())/set_val.shape[0])
+                print('Validation Loss (per pixel): ', sess.run(val_loss.value())/normalization)
                 time2 = datetime.datetime.now()
                 delta = time2-time1
                 print('Epoch Time [seconds]:', delta.seconds)
@@ -1850,7 +1842,7 @@ class PretrainingMAE():
         if run==False:
             raise ValueError
 
-        dir = 'models/' + run
+        dir = 'models/pretraining/' + run
 
         with tf.Session() as sess:
 
@@ -1869,8 +1861,7 @@ class PretrainingMAE():
 
                 depth_pred, l = sess.run([prediction,loss],feed_dict=feed_dict)
                 print_validation_frames(depth_input,depth_pred,depth_label,channel='depth',shape=(60,18))
-                print(np.reshape(depth_pred,(18,60)))
-                print(np.reshape(depth_label,(18,60)))
+
 
                 print('Validation Loss:', l/1080.)
 
@@ -1893,6 +1884,6 @@ pretraining = PretrainingMAE(data_train, data_validate, data_test)
 
 #pretraining.pretrain_shared_semantics()
 
-#pretraining.pretrain_depth_channel()
+pretraining.pretrain_depth_channel()
 
-pretraining.validate_depth(run='20171009-084416')
+#pretraining.validate_depth(run='20171009-084416')

visualization.py
@@ -102,6 +102,7 @@ def print_validation_frames(input_frame,output_frame,label_frame,shape,channel='
     if channel=='red':
         im_shape = (shape[0],shape[1])
 
+
         im_r_input = np.reshape(input_frame,im_shape).T
         im_r_output = np.reshape(output_frame,im_shape).T
         im_r_label = np.reshape(label_frame,im_shape).T
@@ -130,12 +131,21 @@ def print_validation_frames(input_frame,output_frame,label_frame,shape,channel='
     if channel=='depth':
         im_shape = (shape[0],shape[1])
 
+        input_frame = np.asarray(input_frame)
+
+        label_frame = np.asarray(label_frame)
+        label_frame = np.reshape(label_frame,(1,shape[0]*shape[1]))
+
+
+        for i in range(0,input_frame.shape[1]):
+            if input_frame[0,i] == -1.0:
+                input_frame[0,i] = 0
+            if label_frame[0,i] == -1.0:
+                label_frame[0,i] = 0
+
         im_depth_input = np.reshape(input_frame,im_shape).T
         im_depth_output = np.reshape(output_frame,im_shape).T
         im_depth_label = np.reshape(label_frame,im_shape).T
-
-
-
 
         fig,axes = plt.subplots(1,3)
 
@@ -155,6 +165,7 @@ def print_validation_frames(input_frame,output_frame,label_frame,shape,channel='
         plt.close('all')
 
 
+#def full_model_visualiuzation(input_frame,output_frame,label_frame,shape,resolution=(18,60)):
 
 
 