c433ac41febea24a4cd24ea1c2e0493499b20c76
yi liu <yiliu1.ubc@gmail.com>
2017-10-18 12:44:57 +0200

    latest version

--- MAE_pretraining_sep.py
+++ MAE_pretraining_sep.py
@@ -5,16 +5,18 @@ import pandas as pd
 import scipy.io
 import os
 import sys
+
 tf.reset_default_graph()
-batch_size=128
+
+batch_size=100
 hidden_size=1024
-num_epoch=200
+num_epoch=100
+FLAG='training'
 
+print ("start loading the data ......")
 
-
-FLAG='train'
-if (FLAG=='train'):
-    data=process_data('training')
+if (True):
+    data=process_data(FLAG)
     Red_data=data['Red']
     Green_data=data['Green']
     Blue_data=data['Blue']
@@ -25,11 +27,11 @@ if (FLAG=='train'):
     Building_data=data['Building']
     Vegetation_data=data['Vegetation']
     Sky_data=data['Sky']
-    
 
+print ("Finish loading the data !")
+"""################ Start build the separate model####################### """
 
-#RGB Depth model:
-
+"""RGB Depth model:"""
 Red_input=tf.placeholder(tf.float32,shape=[None,1080])
 
 Red_weights=tf.Variable(tf.random_normal(shape=[1080,hidden_size],
@@ -38,23 +40,23 @@ Red_bias=tf.Variable(tf.zeros([1,hidden_size]),name="Red_bias")
 
 Red_hidden=tf.nn.relu(tf.matmul(Red_input,Red_weights)+Red_bias)
 
-Red_outweights=tf.Variable(tf.random_normal(shape=[hidden_size,1080],stddev=0.01),name="Red_outweights")
+Red_outweights=tf.Variable(tf.random_normal(shape=[hidden_size,1080],
+                                  stddev=0.01),name="Red_outweights")
 Red_outbias=tf.Variable(tf.zeros([1,1080]),name="Red_outbias")
 Red_out=tf.nn.sigmoid(tf.matmul(Red_hidden,Red_outweights)+Red_outbias)
 
 
 loss_red=tf.nn.l2_loss(Red_input-Red_out)
+regularization_red=(tf.nn.l2_loss(Red_weights)+tf.nn.l2_loss(Red_outweights))
 
-regularization_red=(tf.nn.l2_loss(Red_weights)
-               +tf.nn.l2_loss(Red_outweights))
+loss_red_t=loss_red+1e-4*regularization_red
 
-loss_red_t=loss_red+1e-5*regularization_red
+optimizer_red=tf.train.AdamOptimizer(learning_rate=1e-6,
+                                      beta1=0.9,beta2=0.999,
+                                      epsilon=1e-8,use_locking=False,
+                                      name='Adam').minimize(loss_red_t)
 
-optimizer_red=tf.train.GradientDescentOptimizer(learning_rate=0.001).minimize(loss_red_t)
-
-# Green model :
-
-
+""" Green model :"""
 
 Green_input=tf.placeholder(tf.float32,shape=[None,1080])
 
@@ -64,22 +66,24 @@ Green_bias=tf.Variable(tf.zeros([1,hidden_size]),name="Green_bias")
 
 Green_hidden=tf.nn.relu(tf.matmul(Green_input,Green_weights)+Green_bias)
 
-Green_outweights=tf.Variable(tf.random_normal(shape=[hidden_size,1080],stddev=0.01),name="Green_outweights")
+Green_outweights=tf.Variable(tf.random_normal(shape=[hidden_size,1080],
+                                  stddev=0.01),name="Green_outweights")
 Green_outbias=tf.Variable(tf.zeros([1,1080]),name="Green_outbias")
 Green_out=tf.nn.sigmoid(tf.matmul(Green_hidden,Green_outweights)+Green_outbias)
 
 
 loss_green=tf.nn.l2_loss(Green_input-Green_out)
 
-regularization_green=(tf.nn.l2_loss(Green_weights)
-                     +tf.nn.l2_loss(Green_outweights))
+regularization_green=tf.nn.l2_loss(Green_weights)+tf.nn.l2_loss(Green_outweights)
 
-loss_green=loss_green+1e-5*regularization_green
+loss_green_t=loss_green+1e-4*regularization_green
 
-optimizer_green=tf.train.GradientDescentOptimizer(learning_rate=0.001).minimize(loss_green)
+optimizer_green=tf.train.AdamOptimizer(learning_rate=1e-6,
+                                      beta1=0.9,beta2=0.999,
+                                      epsilon=1e-8,use_locking=False,
+                                      name='Adam').minimize(loss_green_t)
 
-# ## blue channel
-
+"""Blue Channel:"""
 
 Blue_input=tf.placeholder(tf.float32,shape=[None,1080])
 
@@ -90,23 +94,24 @@ Blue_bias=tf.Variable(tf.zeros([1,hidden_size]),name="Blue_bias")
 
 Blue_hidden=tf.nn.relu(tf.matmul(Blue_input,Blue_weights)+Blue_bias)
 
-Blue_outweights=tf.Variable(tf.random_normal(shape=[hidden_size,1080],stddev=0.01),name="Blue_outweights")
+Blue_outweights=tf.Variable(tf.random_normal(shape=[hidden_size,1080],
+                                   stddev=0.01),name="Blue_outweights")
 Blue_outbias=tf.Variable(tf.zeros([1,1080]),name="Blue_outbias")
 Blue_out=tf.nn.sigmoid(tf.matmul(Blue_hidden,Blue_outweights)+Blue_outbias)
 
 
 loss_blue=tf.nn.l2_loss(Blue_input-Blue_out)
 
-regularization_blue=(tf.nn.l2_loss(Blue_weights)
-                     +tf.nn.l2_loss(Blue_outweights))
+regularization_blue=tf.nn.l2_loss(Blue_weights)+tf.nn.l2_loss(Blue_outweights)
 
-loss_blue=loss_blue+1e-5*regularization_blue
+loss_blue_t=loss_blue+1e-4*regularization_blue
 
-optimizer_blue=tf.train.GradientDescentOptimizer(learning_rate=0.001).minimize(loss_blue)
+optimizer_blue=tf.train.AdamOptimizer(learning_rate=1e-5,
+                                      beta1=0.9,beta2=0.999,
+                                      epsilon=1e-8,use_locking=False,
+                                      name='Adam').minimize(loss_blue_t)
 
-
-
-#### depth channel
+""" depth channel"""
 Depth_input=tf.placeholder(tf.float32,shape=[None,1080])
 Depthmask_input=tf.placeholder(tf.float32,shape=[None,1080])
 
@@ -119,29 +124,22 @@ Depth_bias=tf.Variable(tf.zeros([1,hidden_size]),name="Depth_bias")
 Depth_hidden=tf.nn.relu(tf.matmul(Depth_input,Depth_weights)+Depth_bias)
 
 
-Depth_outweights=tf.Variable(tf.random_normal(shape=[hidden_size,1080],stddev=0.01),name="Depth_outweights")
+Depth_outweights=tf.Variable(tf.random_normal(shape=[hidden_size,1080],
+                                  stddev=0.01),name="Depth_outweights")
 Depth_outbias=tf.Variable(tf.zeros([1,1080]),name="Depth_outbias")
 Depth_out=tf.matmul(Depth_hidden,Depth_outweights)+Depth_outbias
 
-
-
-
 loss_depth=tf.nn.l2_loss(np.multiply(Depth_input,Depthmask_input)-np.multiply(Depth_out,Depthmask_input))
 
-regularization_depth=(tf.nn.l2_loss(Depth_weights)
-                     +tf.nn.l2_loss(Depth_outweights))
+regularization_depth=tf.nn.l2_loss(Depth_weights)+tf.nn.l2_loss(Depth_outweights)
+loss_depth_t=loss_depth+1e-2*regularization_depth
 
-loss_depth_t=loss_depth+1e-4*regularization_depth
-
-optimizer_depth=tf.train.AdamOptimizer(learning_rate=1e-5,
+optimizer_depth=tf.train.AdamOptimizer(learning_rate=1e-4,
                                       beta1=0.9,beta2=0.999,
                                       epsilon=1e-8,use_locking=False,
                                       name='Adam').minimize(loss_depth_t)
 
-
-
-
-######start Ground channel #######
+""" Ground channel """
 Ground_input=tf.placeholder(tf.float32,shape=[None,1080])
 Ground_weights=tf.Variable(tf.random_normal(shape=[1080,hidden_size],
                                    stddev=0.01),name="Ground_weights")
@@ -154,97 +152,84 @@ Ground_outweights=tf.Variable(tf.random_normal(shape=[hidden_size,1080],
 Ground_outbias=tf.Variable(tf.zeros([1,1080]),name="Ground_outbias")
 Ground_out=tf.nn.sigmoid(tf.matmul(Ground_hidden,Ground_outweights)+Ground_outbias)
 
-
 loss_Ground=tf.nn.l2_loss(Ground_input-Ground_out)
-regularization_Ground=(tf.nn.l2_loss(Ground_weights)
-                      +tf.nn.l2_loss(Ground_outweights))
-loss_Ground=loss_Ground+1e-5*regularization_Ground
-optimizer_Ground=tf.train.GradientDescentOptimizer(learning_rate=0.001).minimize(loss_Ground)
-###############
+regularization_Ground=tf.nn.l2_loss(Ground_weights)+tf.nn.l2_loss(Ground_outweights)
+loss_Ground_t=loss_Ground+1e-4*regularization_Ground
+optimizer_Ground=tf.train.AdamOptimizer(learning_rate=1e-6,
+                                      beta1=0.9,beta2=0.999,
+                                      epsilon=1e-8,use_locking=False,
+                                      name='Adam').minimize(loss_Ground_t)
 
-#Objects channel
+"""Objects channel"""
+
 Objects_input=tf.placeholder(tf.float32,shape=[None,1080])
-
 Objects_weights=tf.Variable(tf.random_normal(shape=[1080,hidden_size],
                                    stddev=0.01),name="Objects_weights")
-
 Objects_bias=tf.Variable(tf.zeros([1,hidden_size]),name="Objects_bias")
 
 Objects_hidden=tf.nn.relu(tf.matmul(Objects_input,Objects_weights)+Objects_bias)
-
 Objects_outweights=tf.Variable(tf.random_normal(shape=[hidden_size,1080],
                                     stddev=0.01),name="Objects_outweights")
-
 Objects_outbias=tf.Variable(tf.zeros([1,1080]),name="Objects_outbias")
 
 Objects_out=tf.nn.sigmoid(tf.matmul(Objects_hidden,Objects_outweights)+Objects_outbias)
 
 
 loss_Objects=tf.nn.l2_loss(Objects_input-Objects_out)
-
-regularization_Objects=(tf.nn.l2_loss(Objects_weights)+tf.nn.l2_loss(Objects_outweights))
+regularization_Objects=tf.nn.l2_loss(Objects_weights)+tf.nn.l2_loss(Objects_outweights)
              
 
-loss_Objects=loss_Objects+1e-5*regularization_Objects
+loss_Objects_t=loss_Objects+1e-4*regularization_Objects
 
-optimizer_Objects=tf.train.GradientDescentOptimizer(learning_rate=0.001).minimize(loss_Objects)
-##########
+optimizer_Objects=tf.train.AdamOptimizer(learning_rate=1e-6,
+                                      beta1=0.9,beta2=0.999,
+                                      epsilon=1e-8,use_locking=False,
+                                      name='Adam').minimize(loss_Objects_t)
 
-###Building channels
-
+"""Building channels"""
 Building_input=tf.placeholder(tf.float32,shape=[None,1080])
-
 Building_weights=tf.Variable(tf.random_normal(shape=[1080,hidden_size],
                                    stddev=0.01),name="Building_weights")
-
 Building_bias=tf.Variable(tf.zeros([1,hidden_size]),name="Building_bias")
 
 Building_hidden=tf.nn.relu(tf.matmul(Building_input,Building_weights)+Building_bias)
 
-Building_outweights=tf.Variable(tf.random_normal(shape=[hidden_size,1080],stddev=0.01)
-                                   ,name="Building_outweights")
-
+Building_outweights=tf.Variable(tf.random_normal(shape=[hidden_size,1080],
+                                    stddev=0.01),name="Building_outweights")
 Building_outbias=tf.Variable(tf.zeros([1,1080]),name="Building_outbias")
-
 Building_out=tf.nn.sigmoid(tf.matmul(Building_hidden,Building_outweights)+Building_outbias)
 
+
 loss_Building=tf.nn.l2_loss(Building_input-Building_out)
+regularization_Building=tf.nn.l2_loss(Building_weights)+tf.nn.l2_loss(Building_outweights)
+loss_Building_t=loss_Building+1e-4*regularization_Building
 
-regularization_Building=(tf.nn.l2_loss(Building_weights)
-               +tf.nn.l2_loss(Building_outweights))
+optimizer_Building=tf.train.AdamOptimizer(learning_rate=1e-6,
+                                      beta1=0.9,beta2=0.999,
+                                      epsilon=1e-8,use_locking=False,
+                                      name='Adam').minimize(loss_Building_t)
 
-loss_Building=loss_Building+1e-5*regularization_Building
-
-optimizer_Building=tf.train.GradientDescentOptimizer(learning_rate=0.001).minimize(loss_Building)
-
-###########
-#Sky channels
-
+"""Sky channels"""
 Sky_input=tf.placeholder(tf.float32,shape=[None,1080])
-
 Sky_weights=tf.Variable(tf.random_normal(shape=[1080,hidden_size],
                                    stddev=0.01),name="Sky_weights")
-
 Sky_bias=tf.Variable(tf.zeros([1,hidden_size]),name="Sky_bias")
 
-
 Sky_hidden=tf.nn.relu(tf.matmul(Sky_input,Sky_weights)+Sky_bias)
-
-Sky_outweights=tf.Variable(tf.random_normal(shape=[hidden_size,1080],stddev=0.01)
-                                   ,name="Sky_outweights")
+Sky_outweights=tf.Variable(tf.random_normal(shape=[hidden_size,1080],
+                                   stddev=0.01),name="Sky_outweights")
 Sky_outbias=tf.Variable(tf.zeros([1,1080]),name="Sky_outbias")
-
 Sky_out=tf.nn.sigmoid(tf.matmul(Sky_hidden,Sky_outweights)+Sky_outbias)
-
 loss_Sky=tf.nn.l2_loss(Sky_input-Sky_out)
-regularization_Sky=(tf.nn.l2_loss(Sky_weights)
-               +tf.nn.l2_loss(Sky_outweights))
+regularization_Sky=tf.nn.l2_loss(Sky_weights)+tf.nn.l2_loss(Sky_outweights)
 
-loss_Sky=loss_Sky+1e-5*regularization_Sky
+loss_Sky_t=loss_Sky+1e-4*regularization_Sky
+optimizer_Sky=tf.train.AdamOptimizer(learning_rate=1e-6,
+                                      beta1=0.9,beta2=0.999,
+                                      epsilon=1e-8,use_locking=False,
+                                      name='Adam').minimize(loss_Sky_t)
 
-optimizer_Sky=tf.train.GradientDescentOptimizer(learning_rate=0.001).minimize(loss_Sky)
-#######
-# channel Vegetation
+"""channel Vegetation"""
                                                                                       
 Vegetation_input=tf.placeholder(tf.float32,shape=[None,1080])
 
@@ -254,47 +239,43 @@ Vegetation_weights=tf.Variable(tf.random_normal(shape=[1080,hidden_size],
 Vegetation_bias=tf.Variable(tf.zeros([1,hidden_size]),name="Vegetation_bias")
 Vegetation_hidden=tf.nn.relu(tf.matmul(Vegetation_input,Vegetation_weights)+Vegetation_bias)
 
-Vegetation_outweights=tf.Variable(tf.random_normal(shape=[hidden_size,1080],stddev=0.01)
-                                   ,name="Vegetation_outweights")
-
+Vegetation_outweights=tf.Variable(tf.random_normal(shape=[hidden_size,1080],
+                                    stddev=0.01),name="Vegetation_outweights")
 Vegetation_outbias=tf.Variable(tf.zeros([1,1080]),name="Vegetation_outbias")
-
 Vegetation_out=tf.nn.sigmoid(tf.matmul(Vegetation_hidden,Vegetation_outweights)+Vegetation_outbias)
 loss_Vegetation=tf.nn.l2_loss(Vegetation_input-Vegetation_out)
 
-regularization_Vegetation=(tf.nn.l2_loss(Vegetation_weights)
-               +tf.nn.l2_loss(Vegetation_outweights))
+regularization_Vegetation=tf.nn.l2_loss(Vegetation_weights)+tf.nn.l2_loss(Vegetation_outweights)
 
-loss_Vegetation=loss_Vegetation+1e-5*regularization_Vegetation
+loss_Vegetation_t=loss_Vegetation+1e-4*regularization_Vegetation
 
-optimizer_Vegetation=tf.train.GradientDescentOptimizer(learning_rate=0.001).minimize(loss_Vegetation)
-#####
+optimizer_Vegetation=tf.train.AdamOptimizer(learning_rate=1e-6,
+                                      beta1=0.9,beta2=0.999,
+                                      epsilon=1e-8,use_locking=False,
+                                      name='Adam').minimize(loss_Vegetation_t)
 
-#Finish  Seperate Model  
-#######
-
-
+"""################ Finish  build the separate model####################### """
+print ('Finish  build the separate model!')
+"""################ Start   train  the separate model####################### """
+print ('start training.....')
 ##start train seperate channels :
+
+config = tf.ConfigProto()
+config.gpu_options.per_process_gpu_memory_fraction = 0.5
+
+model_path="../model"
+if not os.path.isdir(model_path):
+    os.mkdir(model_path)
+"""##################                                     ####################"""
 
 init=tf.global_variables_initializer()
 saver=tf.train.Saver()
 train_size=Red_data.shape[0]
 train_indices=range(train_size)
 
-config = tf.ConfigProto()
-config.gpu_options.per_process_gpu_memory_fraction = 0.5
-
-
-par_path="../par/"
-if not os.path.isdir(par_path):
-    os.mkdir(par_path)
-
-model_path="../model"
-if not os.path.isdir(model_path):
-    os.mkdir(model_path)
-
-
-
+#par_path="../par/"
+#if not os.path.isdir(par_path):
+#    os.mkdir(par_path)
 with tf.Session(config=config) as sess:
     
     sess.run(init)
@@ -343,59 +324,10 @@ with tf.Session(config=config) as sess:
         print('Depth loss is :',l_depth)
         print('Depth regularization is :',rg)
 
-      
     saver.save(sess,model_path+"/pretraining_sep.ckpt")
      
     print ('seperately pretraining finished')
 
-    np.save(par_path+"Red_weights",sess.run(Red_weights))
-    np.save(par_path+"Red_bias",sess.run(Red_bias))
-    np.save(par_path+"Red_outweights",sess.run(Red_outweights))
-    np.save(par_path+"Red_outbias",sess.run(Red_outbias))
-    
-    np.save(par_path+"Green_weights",sess.run(Green_weights))
-    np.save(par_path+"Green_bias",sess.run(Green_bias))
-    np.save(par_path+"Green_outweights",sess.run(Green_outweights))
-    np.save(par_path+"Green_outbias",sess.run(Green_outbias))
-    
-    np.save(par_path+"Blue_weights",sess.run(Blue_weights))
-    np.save(par_path+"Blue_bias",sess.run(Blue_bias))
-    np.save(par_path+"Blue_outweights",sess.run(Blue_outweights))
-    np.save(par_path+"Blue_outbias",sess.run(Blue_outbias))
-    
-
-    np.save(par_path+"Depth_weights",sess.run(Depth_weights))
-    np.save(par_path+"Depth_bias",sess.run(Depth_bias))
-    np.save(par_path+"Depth_outweights",sess.run(Depth_outweights))
-    np.save(par_path+"Depth_outbias",sess.run(Depth_outbias))
-
-
-    np.save(par_path+"Ground_weights",sess.run(Ground_weights))
-    np.save(par_path+"Ground_bias",sess.run(Ground_bias))
-    np.save(par_path+"Ground_outweights",sess.run(Ground_outweights))
-    np.save(par_path+"Ground_outbias",sess.run(Ground_outbias))
-    
-    np.save(par_path+"Objects_weights",sess.run(Objects_weights))
-    np.save(par_path+"Objects_bias",sess.run(Objects_bias))
-    np.save(par_path+"Objects_outweights",sess.run(Objects_outweights))
-    np.save(par_path+"Objects_outbias",sess.run(Objects_outbias))
-    
-    np.save(par_path+"Building_weights",sess.run(Building_weights))
-    np.save(par_path+"Building_bias",sess.run(Building_bias))
-    np.save(par_path+"Building_outweights",sess.run(Building_outweights))
-    np.save(par_path+"Building_outbias",sess.run(Building_outbias))
-    
-    np.save(par_path+"Vegetation_weights",sess.run(Vegetation_weights))
-    np.save(par_path+"Vegetation_bias",sess.run(Vegetation_bias))
-    np.save(par_path+"Vegetation_outweights",sess.run(Vegetation_outweights))
-    np.save(par_path+"Vegetation_outbias",sess.run(Vegetation_outbias)) 
-    
-    np.save(par_path+"Sky_weights",sess.run(Sky_weights))
-    np.save(par_path+"Sky_bias",sess.run(Sky_bias))
-    np.save(par_path+"Sky_outweights",sess.run(Sky_outweights))
-    np.save(par_path+"Sky_outbias",sess.run(Sky_outbias)) 
-
 ####################################
-# start train the full shared MAE #
+# Next start train the full shared MAE #
 ####################################
-
