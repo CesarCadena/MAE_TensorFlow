00afce25aed6c6862d1bd9003e8d1a9954689bf0
yi liu <yiliu1.ubc@gmail.com>
2017-10-27 16:05:06 +0200

    update

---
+++ README.md
@@ -0,0 +1,24 @@
+# README #
+
+Multimodal Autoencoders on TensorFlow.
+
+The data for training, validation and testing can be found here:
+
+https://polybox.ethz.ch/index.php/s/CNaOah6Ydq7WrRK
+
+
+
+# How to run  ...
+1.run MAE_pretraining_sep.py  
+2.run MAE_pretraining_sem_shared.py  
+3.run fullMAE.py  
+
+# depth evaluation
+
+for first 1000 test data frames ,  
+with RGBDs input   
+RMSE error is 4.3044  
+absr error is 0.3595  
+processing time for one frame is 20ms on mac laptop.  
+
+
---
+++ depth_error.py
@@ -0,0 +1,48 @@
+# coding: utf-8
+import numpy as np
+def ABSR(truth,prediction):
+
+    T1=truth>0.02
+    T2=prediction>0.02
+    T=np.logical_and(T1,T2)
+    truth=inverse_depth(truth[T])
+    prediction=inverse_depth(prediction[T])
+
+    #T3=truth<50
+    #T4=prediction<50
+    #T=np.logical_and(T3,T4)
+    #prediction=prediction[T]
+    #truth=truth[T]
+
+    n=len(truth)
+    #print("valid data points",n)
+    error=sum(abs(truth-prediction)/truth)
+    abs_error=error/(n+1e-8)
+    
+    return abs_error
+
+
+def inverse_depth(a):
+    b=1.0/a
+    return b
+
+def RMSE(truth,prediction):
+
+    T1=truth>0.02
+    T2=prediction>0.02
+    T=np.logical_and(T1,T2)
+    truth=inverse_depth(truth[T])
+    prediction=inverse_depth(prediction[T])
+
+    #T3=truth<50
+    #T4=prediction<50
+    #T=np.logical_and(T3,T4)
+    #prediction=prediction[T]
+    #truth=truth[T]
+
+    n=len(truth)
+    #print("valid data points",n)
+    error=sum((truth-prediction)**2)/(n+1e-8)
+    rmse_error=np.sqrt(error)
+    
+    return rmse_error
