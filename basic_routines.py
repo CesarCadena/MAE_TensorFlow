18396c1fb4763ffe757387e07e445f8435810736
weders <weders@student.ethz.ch>
2017-11-23 15:15:06 +0100

    implemented get_frames()

--- basic_routines.py
+++ basic_routines.py
@@ -102,3 +102,83 @@ def sem2image(semantic_channel,resolution=(18,60)):
     ims = np.reshape(semantic_channel,(resolution[1],resolution[0])).T
 
     return ims
+
+def get_frames(sequences,data,size_input=1080):
+
+        imr_data = []
+        img_data = []
+        imb_data = []
+        dpt_data = []
+        dpt_msk_data = []
+        gnd_data = []
+        obj_data = []
+        bld_data = []
+        veg_data = []
+        sky_data= []
+
+        zero_padding = np.zeros((size_input,))
+
+
+        for sequence in sequences:
+
+            imr_seq = []
+            img_seq = []
+            imb_seq = []
+            dpt_seq = []
+            dpt_msk_seq = []
+            gnd_seq = []
+            obj_seq = []
+            bld_seq = []
+            veg_seq = []
+            sky_seq = []
+
+            for frame in sequence:
+
+                if frame[0] == -1:
+
+                    imr_seq.append(zero_padding)
+                    img_seq.append(zero_padding)
+                    imb_seq.append(zero_padding)
+                    dpt_seq.append(zero_padding)
+                    dpt_msk_seq.append(zero_padding)
+                    gnd_seq.append(zero_padding)
+                    obj_seq.append(zero_padding)
+                    bld_seq.append(zero_padding)
+                    veg_seq.append(zero_padding)
+                    sky_seq.append(zero_padding)
+
+                else:
+
+                    seq = frame[0]
+                    frm = frame[1]
+
+                    imr_seq.append(data[seq][frm]['xcr1']/255.)
+                    img_seq.append(data[seq][frm]['xcg1']/255.)
+                    imb_seq.append(data[seq][frm]['xcb1']/255.)
+                    dpt_seq.append(data[seq][frm]['xid1'])
+                    dpt_msk_seq.append(data[seq][frm]['xmask1'])
+                    gnd_seq.append((data[seq][frm]['sem1']==1).astype(int))
+                    obj_seq.append((data[seq][frm]['sem1']==2).astype(int))
+                    bld_seq.append((data[seq][frm]['sem1']==3).astype(int))
+                    veg_seq.append((data[seq][frm]['sem1']==4).astype(int))
+                    sky_seq.append((data[seq][frm]['sem1']==5).astype(int))
+
+
+            imr_data.append(copy(imr_seq))
+            img_data.append(copy(img_seq))
+            imb_data.append(copy(imb_seq))
+            dpt_data.append(copy(dpt_seq))
+            dpt_msk_data.append(copy(dpt_msk_seq))
+            gnd_data.append(copy(gnd_seq))
+            obj_data.append(copy(obj_seq))
+            bld_data.append(copy(bld_seq))
+            veg_data.append(copy(veg_seq))
+            sky_data.append(copy(sky_seq))
+
+        return [imr_data,img_data,imb_data,dpt_data,dpt_msk_data,gnd_data,obj_data,bld_data,veg_data,sky_data]
+
+
+
+
+
+
