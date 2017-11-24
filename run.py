ba9ff6296034a905089c78c07c29df7df29935da
weders <weders@student.ethz.ch>
2017-11-24 14:42:16 +0100

    bug fixing

--- run.py
+++ run.py
@@ -11,14 +11,18 @@ from copy import copy
 from MAE import MAE
 
 # Load data for training
+print('load data')
 train_seq, val_seq, test_seq = load_split_data() 
 
+print('generate training and validation data')
 data_train = load_set_data(train_seq)
 data_val = load_set_data(val_seq)
 
+print('initialize model')
 # Define Full Model
 mae = MAE()
 
 # Train MultiModal AutoEncoder
+print('start training')
 mae.train_model(data_train,data_val,load = 'pretrained')
 del data_train, data_val
