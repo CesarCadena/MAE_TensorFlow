d022f4931c294b9baadc94ce9a089da635a81ab7
weders <weders@student.ethz.ch>
2017-12-04 17:31:50 +0100

    no message

--- RecurrentMAE.py
+++ RecurrentMAE.py
@@ -77,7 +77,7 @@ class RecurrentMAE:
 
 
         self.learning_rate = 1e-6
-        self.hm_epochs = 1000
+        self.hm_epochs = 250
 
 
         # variables for overfitting detection
@@ -476,7 +476,7 @@ class RecurrentMAE:
     def network(self, input):
 
         output = RNN_MAE(input[0],input[1],input[2],input[3],input[4],input[5],input[6],input[7],input[8],
-                         n_rnn_steps=self.n_rnn_steps,init_states=self.init_states,option='gated')
+                         n_rnn_steps=self.n_rnn_steps,init_states=self.init_states,option='basic')
         return output
 
     def overfitting_detection(self,val_losses,epoch):
