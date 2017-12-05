56b7779721c4d9aaeb58916ebf5bec023704700a
weders <weders@student.ethz.ch>
2017-12-05 18:37:16 +0100

    no message

--- load_data.py
+++ load_data.py
@@ -435,42 +435,42 @@ def load_set_data(data_seq):
     n_seq = len(data_seq)
 
     dset = []
-    frame =  {'xcrLeft': 0,
-              'xcgLeft': 0,
-              'xcbLeft': 0,
-              'xcrRight': 0,
-              'xcgRight': 0,
-              'xcbRight': 0,
-              'xidsparseLeft': 0,
-              'xidLeft': 0,
-              'xmaskLeft': 0,
-              'xidsparseRight': 0,
-              'xidRight': 0,
-              'xmaskRight': 0,
-              'semLeft':0,
-              'semRight':0}
+    frame =  {'xcr1': 0,
+              'xcg1': 0,
+              'xcb1': 0,
+              'xcr2': 0,
+              'xcg2': 0,
+              'xcb2': 0,
+              'xidsparse1': 0,
+              'xid1': 0,
+              'xmask1': 0,
+              'xidsparse2': 0,
+              'xid2': 0,
+              'xmask2': 0,
+              'sem1':0,
+              'sem2':0}
 
     for i in range(0,n_seq):
         seq = []
         for j in range(0,data_set[i]['xcr'].shape[1]):
-            frame['xcrLeft'] = copy(data_set[i]['xcr'][:,j])
-            frame['xcgLeft'] = copy(data_set[i]['xcg'][:,j])
-            frame['xcbLeft'] = copy(data_set[i]['xcb'][:,j])
+            frame['xcr1'] = copy(data_set[i]['xcr'][:,j])
+            frame['xcg1'] = copy(data_set[i]['xcg'][:,j])
+            frame['xcb1'] = copy(data_set[i]['xcb'][:,j])
 
-            frame['xcrRight'] = copy(data_set[i+n_seq]['xcr'][:,j])
-            frame['xcgRight'] = copy(data_set[i+n_seq]['xcg'][:,j])
-            frame['xcbRight'] = copy(data_set[i+n_seq]['xcb'][:,j])
+            frame['xcr2'] = copy(data_set[i+n_seq]['xcr'][:,j])
+            frame['xcg2'] = copy(data_set[i+n_seq]['xcg'][:,j])
+            frame['xcb2'] = copy(data_set[i+n_seq]['xcb'][:,j])
 
-            frame['xidsparseLeft'] = copy(data_set[i+2*n_seq]['xidsparse'][:,j])
-            frame['xidLeft'] = copy(data_set[i+2*n_seq]['xid'][:,j])
-            frame['xmaskLeft'] = copy(data_set[i+2*n_seq]['xmask'][:,j])
+            frame['xidsparse1'] = copy(data_set[i+2*n_seq]['xidsparse'][:,j])
+            frame['xid1'] = copy(data_set[i+2*n_seq]['xid'][:,j])
+            frame['xmask1'] = copy(data_set[i+2*n_seq]['xmask'][:,j])
 
-            frame['xidsparseRight'] = copy(data_set[i+3*n_seq]['xidsparse'][:,j])
-            frame['xidRight'] = copy(data_set[i+3*n_seq]['xid'][:,j])
-            frame['xmaskRight'] = copy(data_set[i+3*n_seq]['xmask'][:,j])
+            frame['xidsparse2'] = copy(data_set[i+3*n_seq]['xidsparse'][:,j])
+            frame['xid2'] = copy(data_set[i+3*n_seq]['xid'][:,j])
+            frame['xmask2'] = copy(data_set[i+3*n_seq]['xmask'][:,j])
 
-            frame['semLeft'] = copy(data_set[i+4*n_seq]['xss'][:,j])
-            frame['semRight'] = copy(data_set[i+5*n_seq]['xss'][:,j])
+            frame['sem1'] = copy(data_set[i+4*n_seq]['xss'][:,j])
+            frame['sem2'] = copy(data_set[i+5*n_seq]['xss'][:,j])
 
             frame1 = copy(frame)
             seq.append(frame1)
