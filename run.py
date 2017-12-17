33d684bac5449aaa467d8b1ec65be3b7b6920251
weders <weders@student.ethz.ch>
2017-12-17 19:36:33 +0100

    no message

--- run.py
+++ run.py
@@ -17,7 +17,7 @@ data_test = load_frames_data(test_frames)
 print('initialize model')
 # Define Full Model
 #mae = MAE()
-rnn_mae = RecurrentMAE(n_epochs=1000,rnn_option='basic',n_rnn_steps=5,mirroring=True,learning_rate=1e-07)
+rnn_mae = RecurrentMAE(n_epochs=1000,rnn_option='basic',n_rnn_steps=5,mirroring=False,learning_rate=1e-07)
 # Train MultiModal AutoEncoder
 print('start training')
 rnn_mae.train_model(data_train,data_val)
