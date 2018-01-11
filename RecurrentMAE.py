0df6132bcb6ebb6152e49437d001d80da8ca4017
weders <weders@student.ethz.ch>
2018-01-11 13:06:17 +0100

    adapted learning rate

--- RecurrentMAE.py
+++ RecurrentMAE.py
@@ -783,13 +783,11 @@ class RecurrentMAE:
         self.training_cost = cost
 
         global_step = tf.Variable(0,trainable=False)
-        base_rate = self.learning_rate
+        base_rate = 1e-03
         #self.learning_rate = tf.train.exponential_decay(base_rate,global_step,10000, 0.96, staircase=True)
         self.learning_rate = tf.train.piecewise_constant(global_step,
                                                          [50*self.n_batches,100*self.n_batches,150*self.n_batches,200*self.n_batches],
                                                          [base_rate,0.1*base_rate,(0.1**2)*base_rate,(0.1**3)*base_rate])
-
-        self.learning_rate = 1e-05
 
 
         summary_lr = tf.summary.scalar('Learning Rate',self.learning_rate)
