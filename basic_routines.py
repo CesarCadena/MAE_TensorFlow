36da9bcbc3d72ed184ffdff9a7bc7cf1feed6bd8
weders <weders@student.ethz.ch>
2018-01-11 16:00:37 +0100

    bug fixing

--- basic_routines.py
+++ basic_routines.py
@@ -19,15 +19,10 @@ def invert_depth(inv_depth,option='gt'):
     inv_depth = np.reshape(inv_depth,(max(inv_depth.shape),1))
     depth = np.zeros(inv_depth.shape)
 
-    if option == 'gt':
-        for i in range(0,inv_depth.shape[0]):
-            if inv_depth[i] > 1/80.:
-                depth[i] = 1./inv_depth[i]
 
-    if option == 'est':
-        for i in range(0,inv_depth.shape[0]):
-            if inv_depth[i] > 1/80.:
-                depth[i] = 1./inv_depth[i]
+    for i in range(0,inv_depth.shape[0]):
+        if inv_depth[i] > 0.0:
+            depth[i] = 1./inv_depth[i]
 
     return depth
 
