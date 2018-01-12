1ee9f22201a3f23973630b808be43fff294555a3
weders <weders@student.ethz.ch>
2018-01-12 08:40:19 +0100

    bug detection

--- evaluation_functions.py
+++ evaluation_functions.py
@@ -21,17 +21,7 @@ def relative_error(xest,xgt):
     xest = np.reshape(xest,(max(xest.shape),1))
     xgt = np.reshape(xgt,(max(xgt.shape),1))
 
-    T1 = np.ones(xest.shape)
-    T2 = np.ones(xgt.shape)
-
-    for i in range(0,xest.shape[0]):
-        if xest[i] == 0:
-            T1[i] = 0
-        if xgt[i] == 0:
-            T2[i] = 0
-
-    T3 = T1+T2
-    T = np.where(T3==2)[0]
+    T = np.where(xgt < 80)
 
     print(len(T))
 
