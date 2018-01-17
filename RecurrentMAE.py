dcd2b835d185b44574cfedffabe5f010c27647e4
weders <weders@student.ethz.ch>
2018-01-17 20:41:24 +0100

    new configuration

--- RecurrentMAE.py
+++ RecurrentMAE.py
@@ -818,7 +818,7 @@ class RecurrentMAE:
                                                              [base_rate,lr1,lr2,lr3,lr4])
 
         if self.rnn_option == 'gated':
-            base_rate = 1e-04
+            base_rate = 1e-03
             lr1 = 0.1*base_rate
             lr2 = 0.1*lr1
             lr3 = 0.1*lr2
